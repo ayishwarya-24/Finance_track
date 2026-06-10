3379f8c615432bccd9c80d8365a0893b986855ab
Ayishwarya Sivakumar <ayishwarya2406@gmail.com>
2026-06-10 17:02:03 +0400

    Connected Express backend to MySQL

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
+const db = require("./config/db");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("FinanceTrack Backend Running");
